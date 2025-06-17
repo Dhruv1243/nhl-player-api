a5fa308d59d14536b221b9ba33741982248ed541
Dhruv Patel <pateld2253@gmail.com>
2025-06-17 10:02:17 -0400

    Basic creation of team card component.

---
+++ frontend/src/components/TeamCard.jsx
@@ -0,0 +1,22 @@
+function TeamCard({ team }) {
+  function onFavoriteClick() {
+    alert(`clicked`);
+  }
+  return (
+    <div className="team-card">
+      <div className="team-image">
+        <img src={team.image} alt={team.name} />
+        <div className="team-overlay">
+          <button className="favorite-btn" onClick={onFavoriteClick}>
+            👍
+          </button>
+        </div>
+      </div>
+      <div className="team-name">
+        <h3>{team.name}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default TeamCard;
