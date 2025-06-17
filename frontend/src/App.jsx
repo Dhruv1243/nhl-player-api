74c73c66f3741767278bfcf051ad570e329333e1
Dhruv Patel <pateld2253@gmail.com>
2025-06-17 11:10:35 -0400

    Imported react-router-dom and created routes in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import TeamCard from "./components/TeamCard";
+import FacoriteTeams from "./pages/FavoriteTeams.jsx";
 import Home from "./pages/Home.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favoriteTeams" element={<FacoriteTeams />} />
+      </Routes>
+    </main>
   );
 }
 
---
+++ frontend/src/pages/FavoriteTeams.jsx
@@ -0,0 +1,10 @@
+function FacoriteTeams() {
+  return (
+    <div className="favorite-teams">
+      <h2>No Favorite Teams Yet</h2>
+      <p>You can add favorite teams from the home page</p>
+    </div>
+  );
+}
+
+export default FacoriteTeams;
