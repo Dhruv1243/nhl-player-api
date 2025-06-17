2fe4086c5c86620eccfd5f3d46faa16b936c3c5e
Dhruv Patel <pateld2253@gmail.com>
2025-06-17 10:56:33 -0400

    Only the team names that match the search query will now be rendered

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import TeamCard from "../components/TeamCard";
+import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const teams = [
-    { id: 1, teamName: "test" },
-    { id: 2, teamName: "test2" },
-    { id: 3, teamName: "test3" },
+    { id: 1, teamName: "Montreal Canadiens" },
+    { id: 2, teamName: "Toronto Maple Leafs" },
+    { id: 3, teamName: "Edmonton Oilers" },
   ];
 
-  const handleSearch = () => {
-    console.log("search");
+  const handleSearch = (e) => {
+    e.preventDefault(); // this prevent refresh of page so that search bar doesnt clear after every search
+    alert(`Search for ${searchQuery}`);
   };
   return (
     <div className="home">
@@ -17,15 +21,28 @@ function Home() {
           type="text"
           placeholder="Search for teams..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => {
+            setSearchQuery(e.target.value);
+            // console.log(e.target.value); this is only for debugging purposes
+          }}
         />
+
         <button type="submit" className="search-btn">
           Search
         </button>
       </form>
+      {/** 1. loop over teams array 
+       * 2. check if teamName includes 
+      searchQuery
+       * 3. if yes, render TeamCard */}
       <div className="team-grid">
-        {teams.map((team) => (
-          <TeamCard team={team} key={team.id} />
-        ))}
+        {teams.map(
+          (team) =>
+            team.teamName.toLowerCase().includes(searchQuery.toLowerCase()) && (
+              <TeamCard team={team} key={team.id} />
+            )
+        )}
       </div>
     </div>
   );
